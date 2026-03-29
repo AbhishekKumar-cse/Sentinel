7ba644d6854a907602d7dd40d01fc141a5a7c1c2
Abhishek Kumar <abhishek.cse.dev@gmail.com>
2026-03-29 16:46:32 +0000

    feat : Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-225289741-33be7",
+  "appId": "1:790098009330:web:e29fa58fb688de660a846b",
+  "apiKey": "AIzaSyArTljh_W7mPn936YVKLp74fljekzkcYrw",
+  "authDomain": "studio-225289741-33be7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "790098009330"
+};
